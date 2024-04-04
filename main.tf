1f83da3253b091afbac244eaccb9fe0f9c119a20
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-04 11:06:05 +0200

    backend

--- main.tf
+++ main.tf
@@ -5,16 +5,6 @@ terraform {
       version = ">= 3.8.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-githubtfstates"
-    storage_account_name = "miszelaudit"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    subscription_id      = "f80611eb-0851-4373-b7a3-f272906843c4"
-    tenant_id            = var.tenant_id
-  }
 }
 
 provider "azurerm" {
