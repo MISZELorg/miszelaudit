9e776679e83f3ec8800609dd18bdb3cd475731d2
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-09 15:00:39 +0200

    add kv

--- main.tf
+++ main.tf
@@ -27,6 +27,46 @@ resource "azurerm_resource_group" "rg-audit" {
   location = var.location
 }
 
+resource "azurerm_key_vault" "kv-audit-logs" {
+  name                            = var.audit-kv_name
+  location                        = azurerm_resource_group.rg-audit.location
+  resource_group_name             = azurerm_resource_group.rg-audit.name
+  sku_name                        = "standard"
+  tenant_id                       = data.azurerm_client_config.current.id #"48c383d8-47c5-48f9-9e8b-afe4f2519054"
+  enabled_for_deployment          = false
+  enabled_for_disk_encryption     = true
+  enabled_for_template_deployment = true
+  soft_delete_retention_days      = 90
+  purge_protection_enabled        = false # set to false once ready
+  enable_rbac_authorization       = true
+  public_network_access_enabled   = false
+  depends_on                      = [azurerm_resource_group.rg-audit]
+
+  network_acls {
+    bypass                     = "AzureServices"
+    default_action             = "Deny"
+    virtual_network_subnet_ids = []
+    ip_rules                   = ["84.10.55.110/32"]
+  }
+}
+
+# resource "azurerm_user_assigned_identity" "uami-audit-logs" {
+#   name                = var.audit-uami_name
+#   location            = azurerm_resource_group.rg-audit.location
+#   resource_group_name = azurerm_resource_group.rg-audit.name
+#   depends_on          = [
+#     azurerm_resource_group.rg-audit,
+#     azurerm_key_vault.kv-audit-logs
+#   ]
+# }
+
+# resource "azurerm_role_assignment" "RBAC-AZTF-kv-uami-audit-logs" {
+#   scope                = azurerm_storage_account.sa-audit-logs.id
+#   role_definition_name = "Key Vault Crypto Service Encryption User"
+#   principal_id         = azurerm_user_assigned_identity.uami-audit-logs.id
+#   depends_on           = [azurerm_user_assigned_identity.uami-audit-logs]
+# }
+
 resource "azurerm_storage_account" "sa-audit-logs" {
   name                             = var.audit-sa_name
   resource_group_name              = azurerm_resource_group.rg-audit.name
@@ -59,27 +99,27 @@ resource "azurerm_role_assignment" "RBAC-AZTFREAD-sa-audit-logs" {
   depends_on           = [azurerm_storage_account.sa-audit-logs]
 }
 
-resource "azurerm_storage_container" "cont-insights-activity-logs" {
-  name                  = "insights-activity-logs"
-  storage_account_name  = azurerm_storage_account.sa-audit-logs.name
-  container_access_type = "private"
-  depends_on = [
-    azurerm_resource_group.rg-audit,
-    azurerm_storage_account.sa-audit-logs,
-    azurerm_role_assignment.RBAC-AZTF-sa-audit-logs
-  ]
-}
+# resource "azurerm_storage_container" "cont-insights-activity-logs" {
+#   name                  = "insights-activity-logs"
+#   storage_account_name  = azurerm_storage_account.sa-audit-logs.name
+#   container_access_type = "private"
+#   depends_on = [
+#     azurerm_resource_group.rg-audit,
+#     azurerm_storage_account.sa-audit-logs,
+#     azurerm_role_assignment.RBAC-AZTF-sa-audit-logs
+#   ]
+# }
 
-resource "azurerm_storage_container" "cont-insights-operational-logs" {
-  name                  = "insights-operational-logs"
-  storage_account_name  = azurerm_storage_account.sa-audit-logs.name
-  container_access_type = "private"
-  depends_on = [
-    azurerm_resource_group.rg-audit,
-    azurerm_storage_account.sa-audit-logs,
-    azurerm_role_assignment.RBAC-AZTF-sa-audit-logs
-  ]
-}
+# resource "azurerm_storage_container" "cont-insights-operational-logs" {
+#   name                  = "insights-operational-logs"
+#   storage_account_name  = azurerm_storage_account.sa-audit-logs.name
+#   container_access_type = "private"
+#   depends_on = [
+#     azurerm_resource_group.rg-audit,
+#     azurerm_storage_account.sa-audit-logs,
+#     azurerm_role_assignment.RBAC-AZTF-sa-audit-logs
+#   ]
+# }
 
 # # resource "azurerm_log_analytics_workspace" "log-audit-logs" {
 # #   name                = var.audit-logs_name
@@ -90,32 +130,5 @@ resource "azurerm_storage_container" "cont-insights-operational-logs" {
 # #   depends_on          = [azurerm_resource_group.rg-audit]
 # # }
 
-# resource "azurerm_key_vault" "kv-audit-logs" {
-#   name                            = var.audit-kv_name
-#   location                        = azurerm_resource_group.rg-audit.location
-#   resource_group_name             = azurerm_resource_group.rg-audit.name
-#   sku_name                        = "standard"
-#   tenant_id                       = data.azurerm_client_config.current.id #"48c383d8-47c5-48f9-9e8b-afe4f2519054"
-#   enabled_for_deployment          = false
-#   enabled_for_disk_encryption     = true
-#   enabled_for_template_deployment = true
-#   soft_delete_retention_days      = 90
-#   purge_protection_enabled        = false # set to false once ready
-#   enable_rbac_authorization       = true
-#   public_network_access_enabled   = false
-#   depends_on                      = [azurerm_resource_group.rg-audit]
 
-#   network_acls {
-#     bypass                     = "AzureServices"
-#     default_action             = "Deny"
-#     virtual_network_subnet_ids = []
-#     ip_rules                   = ["84.10.55.110"]
-#   }
-# }
 
-# # resource "azurerm_user_assigned_identity" "uami-audit-logs" {
-# #   name                = var.audit-uami_name
-# #   location            = azurerm_resource_group.rg-audit.location
-# #   resource_group_name = azurerm_resource_group.rg-audit.name
-# #   depends_on          = [azurerm_resource_group.rg-audit]
-# # }
