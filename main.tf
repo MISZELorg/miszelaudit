2895a7eb33ac0ab5102c17be15e5c6a26234b3cd
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-18 15:33:26 +0200

    kvmodule

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ module "keyvault" {
   rg_name     = var.rg_name
   location    = var.location
   kv_name     = var.kv_name
-  kv_sku_name = var.kv_name
+  kv_sku_name = var.kv_sku_name
   kv_ip_rules = var.kv_ip_rules
 
   depends_on = [
