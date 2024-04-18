dafc9768302653b56bbf642f95bc5482bc82b6a7
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-18 15:50:04 +0200

    adduami

--- main.tf
+++ main.tf
@@ -145,3 +145,12 @@ module "keyvault" {
   ]
 }
 
+resource "azurerm_user_assigned_identity" "uami" {
+  name                = var.uami_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  depends_on = [
+    module.rg,
+    module.keyvault
+  ]
+}
