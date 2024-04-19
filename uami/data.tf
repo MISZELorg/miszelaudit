6224d78d37299819d41aa70490f3d90faf12770e
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-19 10:35:45 +0200

    adduami

--- uami/data.tf
+++ uami/data.tf
@@ -1,4 +1,5 @@
 data "azurerm_user_assigned_identity" "uami-logs" {
   name                = var.uami_name
   resource_group_name = var.rg_name
+  depends_on          = [azurerm_user_assigned_identity.uami]
 }
