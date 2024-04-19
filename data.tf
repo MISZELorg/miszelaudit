aefd79cddbd5713bd7c6a2e61c2044b76d4f888b
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-19 14:47:48 +0200

    cleanold

--- data.tf
+++
@@ -1,5 +0,0 @@
-# data "azurerm_user_assigned_identity" "uami-audit-logs" {
-#   name                = var.audit-uami_name
-#   resource_group_name = azurerm_resource_group.rg-audit.name
-# }
-
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-# output "uami-audit-logs_id" {
-#   value = data.azurerm_user_assigned_identity.uami-audit-logs.principal_id
-# }
