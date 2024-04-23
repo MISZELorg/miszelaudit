3d6f9cb764d8ca8838d71a8711a122b018d88118
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-23 10:45:48 +0200

    fixid6

--- sa/main.tf
+++ sa/main.tf
@@ -7,16 +7,6 @@ resource "azurerm_storage_account" "sa-logs" {
   cross_tenant_replication_enabled = false
   allow_nested_items_to_be_public  = false
 
-  identity {
-    type = "SystemAssigned"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      customer_managed_key
-    ]
-  }
-
   network_rules {
     default_action             = "Allow"
     virtual_network_subnet_ids = []
@@ -25,11 +15,11 @@ resource "azurerm_storage_account" "sa-logs" {
 
 }
 
-resource "azurerm_storage_account_customer_managed_key" "cmk-logs" {
-  storage_account_id = azurerm_storage_account.sa-logs.id
-  key_vault_id       = var.keyvault_id
-  key_name           = var.key_name
-}
+# resource "azurerm_storage_account_customer_managed_key" "cmk-logs" {
+#   storage_account_id = azurerm_storage_account.sa-logs.id
+#   key_vault_id       = var.keyvault_id
+#   key_name           = var.key_name
+# }
 
 resource "azurerm_storage_container" "cont-insights-activity-logs" {
   name                  = "insights-activity-logs"
